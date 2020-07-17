d34a5f35e6dfa9ac28a79521b236d130cc9522a2
Daylan Quinn <daylanq10@gmail.com>
2020-07-17 15:53:26 -0500

    moble-4

---
+++ Source/hardware/src/main/java/com/example/vijaya/androidhardware/StorageActivity.java
@@ -0,0 +1,113 @@
+package com.example.vijaya.androidhardware;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StorageActivity extends AppCompatActivity {
+    EditText inputText;
+    TextView response;
+    Button saveButton,readButton;
+
+    private String filename = "SampleFile.txt";
+    private String filepath = "MyFileStorage";
+    File myExternalFile;
+    String myData = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storage);
+
+        inputText = (EditText) findViewById(R.id.id_txt_mycontent);
+        response = (TextView) findViewById(R.id.response);
+
+
+        saveButton = (Button) findViewById(R.id.id_btn_save);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fos = new FileOutputStream(myExternalFile);
+                    fos.write(inputText.getText().toString().getBytes());
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                inputText.setText("");
+                response.setText("When Open button will output here");
+                Toast.makeText(getApplicationContext(), "Text saved", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        readButton = (Button) findViewById(R.id.id_btn_display);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileInputStream fis = new FileInputStream(myExternalFile);
+                    DataInputStream in = new DataInputStream(fis);
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(in));
+                    String strLine;
+                    while ((strLine = br.readLine()) != null) {
+                        myData = myData + strLine;
+                    }
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                response.setText(myData);
+                Toast.makeText(getApplicationContext(), "Saved text shown", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        if (!isExternalStorageAvailable() || isExternalStorageReadOnly()) {
+            saveButton.setEnabled(false);
+        }
+        else {
+            myExternalFile = new File(getExternalFilesDir(filepath), filename);
+        }
+
+
+    }
+    private static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    public void redirectToHome(View v) {
+        Intent redirect = new Intent(StorageActivity.this, MainActivity.class);
+        Toast.makeText(this, "return home", Toast.LENGTH_LONG).show();
+        startActivity(redirect);
+    }
+}
+
